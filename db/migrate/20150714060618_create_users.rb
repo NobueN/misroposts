e2f727db0bf1f935fc180ec1613b75749e41cae6
nobuen <nobuen@gmail.com>
2015-07-14 06:12:26 +0000

    add user model

---
+++ db/migrate/20150714060618_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+      
+      t.index :email, unique: true #この行は追加
+      
+    end
+  end
+end
